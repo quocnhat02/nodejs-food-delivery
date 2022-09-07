1cbe82ff722f1daded285ef9cb40ae82f5900a53
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-07 19:15:37 +0700

    creating a simple web server 1

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+
+////////////////////////////////
+// FILE
 
 // Blocking, synchronous
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -9,15 +13,25 @@ const fs = require("fs");
 // console.log("File written!");
 
 // Non-blocking, asynchronous
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("ERROR!");
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR!");
 
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
 
-    fs.writeFile("./txt/final.txt", `${data2}\nHello World`, "utf-8", (err) => {
-      console.log("Your file has been written");
-    });
-  });
+//     fs.writeFile("./txt/final.txt", `${data2}\nHello World`, "utf-8", (err) => {
+//       console.log("Your file has been written");
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
 });
-console.log("Will read file!");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
