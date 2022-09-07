b0de8ed17fbab84ba82c71778ccff0472da87cc2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-07 19:56:50 +0700

    building a simple api 2

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ const http = require("http");
 
 ////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -37,11 +41,8 @@ const server = http.createServer((req, res) => {
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
   } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, { "Content-type": "application/json" });
-      res.end(data);
-    });
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
