c913f66ea740a5e2c290d97a1457a361e752f19a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 14:57:44 +0700

    handling post request 2

--- app.js
+++ app.js
@@ -34,7 +34,10 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours', (req, res) => {});
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
+});
 
 const port = 4000;
 app.listen(port, () => {
