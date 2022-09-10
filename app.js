c69f4609568aeabb1faeb52f4b3d68e6b9e617c5
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 14:17:49 +0700

    setting up express and basic routing 4

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side!');
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
 });
 
 const port = 4000;
