ce4a1de3e200ff387b2dd317f90d3d0c98bc9386
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 19:25:49 +0700

    a better file structure 4

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
-// const tourController = require('../controllers/tourController');
-const {
-  getAllTours,
-  getTour,
-  createTour,
-  updateTour,
-  deleteTour,
-} = require('../controllers/tourController');
+const tourController = require('../controllers/tourController');
+// const {getAllTours, createTour, getTour, updateTour, deleteTour} = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.route('/').get(getAllTours).post(createTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
