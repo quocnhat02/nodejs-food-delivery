9a057273ff648fbba7ac265088219ce5ca1f2692
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 14:50:45 +0700

    starting our api_handling get request 2

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 // });
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-date/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 app.get('/api/v1/tours', (req, res) => {
