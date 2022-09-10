8a5c0113fb8a92c5596ada846b2594b0d94c0e22
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 19:31:58 +0700

    a better file structure 6

--- app.js
+++ app.js
@@ -26,13 +26,7 @@ app.use((req, res, next) => {
 });
 
 // 2.ROUTES
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 3.SERVER
-
-const port = 4000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+module.exports = app;
