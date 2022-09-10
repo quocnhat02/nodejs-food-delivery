7e10fea8cb88766729eb0b45fe9eaec5c79fc1cc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 14:14:18 +0700

    setting up express and basic routing 3

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from the server side!');
+});
+
 const port = 4000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
