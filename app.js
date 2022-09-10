16b410a2f25e0a16073fe4153cd735543987c077
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 15:06:55 +0700

    handling post request 3

--- app.js
+++ app.js
@@ -35,7 +35,26 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
+  //   console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
   res.send('Done');
 });
 
