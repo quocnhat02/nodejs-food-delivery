854e3a0d9d7a0fc4442a633c6802a6f3a5d7d154
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 16:17:02 +0700

    refactoring our routes 8

--- app.js
+++ app.js
@@ -106,9 +106,9 @@ const deleteTour = (req, res) => {
 
 // app.get('/api/v1/tours', getAllTours);
 // app.post('/api/v1/tours', createTour);
-app.get('/api/v1/tours/:id', getTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
