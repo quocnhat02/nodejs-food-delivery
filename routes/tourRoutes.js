be82be1ef1f8651844aeaf397791c9f94f860e21
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 19:38:26 +0700

    param middleware 1

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,11 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
+router.param('id', (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  next();
+});
+
 router
   .route('/')
   .get(tourController.getAllTours)
