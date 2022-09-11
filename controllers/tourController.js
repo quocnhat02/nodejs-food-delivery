3745ec3ab2de3c708002c681e1d9613810e21680
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-11 12:25:26 +0700

    reading documents 1

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,22 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestAt: req.requestTime,
-    // resultS: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      resultS: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.getTour = (req, res) => {
