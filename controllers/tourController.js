b64a4cc62ef9945de6b8321cc26d1bc558c41694
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-11 12:21:11 +0700

    another way of creating documents 4

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,7 +41,7 @@ exports.createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: error,
+      message: 'Invalid data sent!',
     });
   }
 };
