6516c6ffa9d9d675011d96ec5a5c7ff41660cd9d
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-17 23:22:55 +0700

    buildinga simple api 4

--- index.js
+++ index.js
@@ -45,6 +45,14 @@ const url = require('url');
 
 //////////////////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(
+  `${__dirname}/dev-data/data.json`,
+  'utf-8',
+  (err, data) => {}
+);
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === '/' || pathName === '/overview') {
@@ -52,13 +60,10 @@ const server = http.createServer((req, res) => {
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT');
   } else if (pathName === '/api') {
-    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, {
-        'Content-type': 'application/json',
-      });
-      res.end(data);
+    res.writeHead(200, {
+      'Content-type': 'application/json',
     });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
