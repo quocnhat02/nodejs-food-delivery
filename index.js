052b15727339f6a421ffa9645b21aa87c61af144
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-17 21:29:54 +0700

    reading and writing asynchronous 1

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
-console.log(textIn);
+// console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()} `;
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()} `;
 
-fs.writeFileSync("./txt/output.txt", textOut, "utf-8");
+// fs.writeFileSync("./txt/output.txt", textOut, "utf-8");
 
-console.log("File written!");
+// console.log("File written!");
+
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+console.log("Will read file!");
