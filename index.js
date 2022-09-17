085c242d655641f5484aae01d6a38e91c4cf68c2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-18 00:11:06 +0700

    buildinga html templa 2

--- index.js
+++ index.js
@@ -46,6 +46,22 @@ const url = require('url');
 //////////////////////////////////////////////
 // SERVER
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not_organic');
+  }
+  return output;
+};
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
@@ -67,7 +83,14 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       'Content-type': 'text/html',
     });
-    res.end(tempOverview);
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+
+    const output = tempOverview.replace('%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
 
     // Product page
   } else if (pathName === '/product') {
