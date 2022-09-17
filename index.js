65d95c78f13c7bf252f2620757721672e48a88d8
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-17 23:56:50 +0700

    buildinga html templa 1

--- index.js
+++ index.js
@@ -46,20 +46,41 @@ const url = require('url');
 //////////////////////////////////////////////
 // SERVER
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the OVERVIEW');
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    res.end(tempOverview);
+
+    // Product page
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT');
+
+    // API
   } else if (pathName === '/api') {
     res.writeHead(200, {
       'Content-type': 'application/json',
     });
     res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
