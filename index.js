3c710a866688c404a68b2bf76b05f8c86e81ea6f
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-17 22:30:52 +0700

    routing 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 //////////////////////////////////////////////
 // FILES
@@ -45,6 +46,7 @@ const http = require('http');
 //////////////////////////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   res.end('Hello from the server!');
 });
 
