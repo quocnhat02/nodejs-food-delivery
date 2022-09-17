00ee2e7138ac0289576c87005ab753bd18f3a6bb
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-17 21:42:44 +0700

    reading and writing asynchronous 5

--- index.js
+++ index.js
@@ -13,12 +13,24 @@ const fs = require("fs");
 
 // Non-blocking, asynchronous way
 fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) {
+    return console.log("ERROR!");
+  }
   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    if (err) {
+      return console.log("ERROR!");
+    }
     console.log(data2);
     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      if (err) {
+        return console.log("ERROR!");
+      }
       console.log(data3);
 
       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        if (err) {
+          return console.log("ERROR!");
+        }
         console.log("Your file has been written");
       });
     });
