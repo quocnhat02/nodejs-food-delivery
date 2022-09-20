0b5c7da363caa16b6d22a5e033384f8e296ea0f9
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-20 19:22:22 +0700

    modules 14

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const os = require('os');
 // info about current user
 const user = os.userInfo();
 console.log(user);
+
+// method returns the system uptime in seconds
+console.log(`The system Uptime is ${os.uptime()} seconds`);
