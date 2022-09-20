9d6ecc1c620d2b6871f2f008b01071261d8eb14a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-20 20:25:57 +0700

    modules 28

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { readFile, writeFile } = require('fs');
 
-readFile('./content/first.txt', (err, data) => {
+readFile('./content/first.txt', 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
     return;
