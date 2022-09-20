3de571cbcf7c1a82d26899ba351cb406a88d507e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-20 19:24:56 +0700

    modules 15

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ console.log(user);
 
 // method returns the system uptime in seconds
 console.log(`The system Uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+
+console.log(currentOS);
