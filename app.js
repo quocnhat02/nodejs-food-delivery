1d124910b9c72edd1c9be0dad63f6b39a97e0073
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-20 19:10:52 +0700

    modules 10

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const names = require('./names');
 const { sayHi } = require('./utils');
 const data = require('./alternative-flavor');
+require('./mind-grenade');
 
-sayHi('susan');
-sayHi(names.john);
-sayHi(names.peter);
+// sayHi('susan');
+// sayHi(names.john);
+// sayHi(names.peter);
