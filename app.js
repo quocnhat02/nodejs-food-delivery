be83d39f67057b0b8746a489cf937e0eda8af38b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-20 21:05:54 +0700

    modules 35

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.write('Welcome to our home page');
-  res.end();
+  if (req.url === '/') {
+    res.end('Welcome to our home page');
+  }
+  if (req.url === '/about') {
+    res.end('Here is our short history');
+  }
 });
 
 server.listen(5000);
