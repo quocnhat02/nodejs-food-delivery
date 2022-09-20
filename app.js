5864c58ca1d462060ba80bafcb87406511a18d12
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-20 18:46:33 +0700

    globals 3

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log('current directory: ' + __dirname);
+console.log('current filename: ' + __filename);
 
-setInterval(() => {
-  console.log('Hello World');
-}, 1000);
+// setInterval(() => {
+//   console.log('Hello World');
+// }, 1000);
