d977ab525f50662fe75c9c9a40f477d923560703
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 15:20:54 +0700

    express 4

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('user hit the server');
-  res.end('home page');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Home Page</h1>');
+  res.end();
 });
 
 server.listen(5000);
