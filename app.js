095217e5045fb5a0972a7aa7a47e3516a7f6bb2e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 18:00:38 +0700

    express 16

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Home Page');
-});
-
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page');
+  res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
 });
 
 app.all('*', (req, res) => {
-  res.status(404).send('<h1>Resource not found</h1>');
+  res.status(404).send('Resource not found');
 });
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000');
+  console.log(`Server is listening on port 5000...`);
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
