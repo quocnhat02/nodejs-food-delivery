4890d81dcb6998a0753d7ea4c385c8e57cbcce01
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 11:30:13 +0700

    modules 41

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ const getText = (path) => {
   });
 };
 
-getText('./content/first.txt').then((result) =>
-  console.log(result).catch((err) => console.log(err))
-);
+// getText('./content/first.txt').then((result) =>
+//   console.log(result).catch((err) => console.log(err))
+// );
+
+const start = async () => {
+  try {
+    const first = await getText('./content/first.txt');
+    console.log(first);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
