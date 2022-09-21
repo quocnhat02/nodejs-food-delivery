cc66d4c2bf8380d55354116deada2f0a324dace0
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 11:00:12 +0700

    modules 38

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-const { readFile } = require('fs');
-
-console.log('started a first task');
-
-readFile('./content/first.txt', 'utf-8', (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(result);
-  console.log('completed first task');
-});
-
-console.log('starting next task');
+console.log('first');
+setTimeout(() => {
+  console.log('second');
+}, 0);
+console.log('third');
