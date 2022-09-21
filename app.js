7f22f2608ead649dc3c7f7a3922d438d33bc0ba1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 23:16:28 +0700

    express 43

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
 const express = require('express');
-const morgan = require('morgan');
-const { logger } = require('./logger');
-const { authorize } = require('./authorize');
 
 const app = express();
 
-// req => middleware => res
+let { people } = require('./data');
 
-// app.use([logger, authorize]);
-app.use(morgan('combined'));
-// app.use(express.static('./public'));
-
-// api/home/about/products
-app.get('/', (req, res) => {
-  res.send('Home');
-});
-
-app.get('/about', (req, res) => {
-  res.send('About');
-});
-
-app.get('/api/products', (req, res) => {
-  res.send('Products');
-});
-
-app.get('/api/items', (req, res) => {
-  console.log(req.user);
-  res.send('Items');
+app.get('/api/people', (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: people,
+  });
 });
 
 app.listen(5000, () => {
