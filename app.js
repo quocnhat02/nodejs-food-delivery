eba66764643509c1cde15d3d1fabf62769f41b3b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 20:32:46 +0700

    express 27

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const { products } = require('./data');
 
 app.get('/', (req, res) => {
-  res.send('<h1>Home page</h1><a href="/api/products">products</a>');
+  return res.send('<h1>Home page</h1><a href="/api/products">products</a>');
 });
 
 app.get('/api/products', (req, res) => {
@@ -13,7 +13,7 @@ app.get('/api/products', (req, res) => {
     const { id, name, image } = product;
     return { id, name, image };
   });
-  res.json(newProducts);
+  return res.json(newProducts);
 });
 
 app.get('/api/products/:productID', (req, res) => {
@@ -28,7 +28,11 @@ app.get('/api/products/:productID', (req, res) => {
   if (!singleProduct) {
     return res.status(404).send('Product does not exist');
   }
-  res.json(singleProduct);
+  return res.json(singleProduct);
+});
+
+app.get('/api/products/:productID/reviews/reviewID', (req, res) => {
+  return res.send('Hello');
 });
 
 app.listen(5000, () => {
