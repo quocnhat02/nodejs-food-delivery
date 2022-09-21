75908824d9d45c17e13e5d00d3932991f0c17bdc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 20:28:03 +0700

    express 25

--- app.js
+++ app.js
@@ -17,9 +17,14 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/products/:productID', (req, res) => {
-  console.log(req);
-  console.log(req.params);
-  const singleProduct = products.find((product) => product.id === 1);
+  // console.log(req);
+  // console.log(req.params);
+
+  const { productID } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
   res.json(singleProduct);
 });
 
