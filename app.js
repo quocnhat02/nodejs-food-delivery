88f71655140067d3eaeccd959e1f5b1f54d9eb62
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 11:21:52 +0700

    modules 40

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const { readFile } = require('fs');
 
-readFile('./content/first.txt', 'utf-8', (err, data) => {
-  if (err) {
-    return;
-  } else {
-    console.log(data);
-  }
-});
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+getText('./content/first.txt').then((result) =>
+  console.log(result).catch((err) => console.log(err))
+);
