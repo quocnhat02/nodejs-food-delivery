f6fc78f7b3a29459048a4150e42cffc227472a59
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 13:15:02 +0700

    modules 48

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const { createReadStream } = require('fs');
+const http = require('http');
+const fs = require('fs');
 
-const stream = createReadStream('./content/big.txt', {
-  highWaterMark: 90000,
-  encoding: 'utf-8',
-});
-
-stream.on('data', (result) => {
-  console.log(result);
-});
-
-stream.on('error', (err) => {
-  console.log(err);
+http.createServer((req, res) => {
+  const fileStream = fs.createReadStream('./content/big.txt', 'utf-8');
+  fileStream.on('open', () => {
+    fileStream.pipe(res);
+  });
+  fileStream.on('error', (err) => {
+    res.end(err);
+  });
 });
