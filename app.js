845e0a92f595852023cee9455b4c78ca9f5871c5
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 22:23:45 +0700

    express 37

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ const app = express();
 
 // req => middleware => res
 
-app.get('/', logger, (req, res) => {
+app.use('/api', logger);
+
+// api/home/about/products
+app.get('/', (req, res) => {
   res.send('Home');
 });
 
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
   res.send('About');
 });
 
-app.get('/api/products', logger, (req, res) => {
+app.get('/api/products', (req, res) => {
   res.send('Products');
 });
 
-app.get('/api/items', logger, (req, res) => {
+app.get('/api/items', (req, res) => {
   res.send('Items');
 });
 
