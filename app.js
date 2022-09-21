a6eb9c9d5ce68be3f3f4948d95e1481fb2340a23
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 15:46:44 +0700

    express 7

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   if (url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Home Page</h1>');
+    res.write(homePage);
     res.end();
   } else if (url === '/about') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
