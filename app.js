6169d0e2b4119b7775cd1f7a7c6984d6abce03e4
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 12:44:59 +0700

    modules 44

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ customEmitter.on('response', () => {
   console.log(`data received`);
 });
 
+customEmitter.on('response', () => {
+  console.log(`some other logic here`);
+});
+
 customEmitter.emit('response');
