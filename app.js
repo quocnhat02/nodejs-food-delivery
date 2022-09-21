59268d6b9e5783a207277e50162535b8c660cdc1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 22:19:46 +0700

    express 36

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get('/about', logger, (req, res) => {
   res.send('About');
 });
 
+app.get('/api/products', logger, (req, res) => {
+  res.send('Products');
+});
+
+app.get('/api/items', logger, (req, res) => {
+  res.send('Items');
+});
+
 app.listen(5000, () => {
   console.log('Server is listening on port 5000...');
 });
