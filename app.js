706d2261eb5c0f98e288de66daa87762265b7a2a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 18:52:03 +0700

    express 20

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-});
-
-app.all('*', (req, res) => {
-  res.status(404).send('Resource not found');
+  res.json([{ name: 'john' }, { name: 'susan' }]);
 });
 
 app.listen(5000, () => {
-  console.log(`Server is listening on port 5000...`);
+  console.log('Server is listening on port 5000...');
 });
