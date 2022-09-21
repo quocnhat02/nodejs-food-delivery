24bc195ab24f2762fcca739145ab4b2452011ed0
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 12:35:19 +0700

    modules 43

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-const { readFile } = require('fs');
+const EventEmitter = require('events');
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+const customEmitter = new EventEmitter();
 
-// getText('./content/first.txt').then((result) =>
-//   console.log(result).catch((err) => console.log(err))
-// );
+customEmitter.on('response', () => {
+  console.log(`data received`);
+});
 
-const start = async () => {
-  try {
-    const first = await getText('./content/first.txt');
-    const second = await getText('./content/second.txt');
-    console.log(first, second);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
+customEmitter.emit('response');
