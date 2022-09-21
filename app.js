3c8d37317e2f702519d106f5bada7d6618839cd0
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 17:25:58 +0700

    express 14

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
 app.listen(5000, () => {
   console.log('Server is listening on port 5000');
 });
