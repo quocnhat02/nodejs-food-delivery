f96b8c21cf5169c469119b9fe71255c501022b40
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-21 22:16:41 +0700

    express 34

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const logger = (req, res, next) => {
   const url = req.url;
   const time = new Date().getFullYear();
   console.log(method, url, time);
-  res.send('Testing');
+  next();
 };
 
 app.get('/', logger, (req, res) => {
   res.send('Home');
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', logger, (req, res) => {
   res.send('About');
 });
 
