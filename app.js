ba16a4ed9ed7cd52293cdb328be61ba579c77ff7
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-22 22:15:18 +0700

    task manager api 3

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+// routes
+app.get('/hello', (req, res) => {
+  return res.send('Task manager API');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
