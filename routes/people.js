d2ac0af50072daa2df421d126a46c8f1d72d40a9
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-22 21:59:29 +0700

    express 64

--- routes/people.js
+++ routes/people.js
@@ -10,14 +10,14 @@ const {
 
 const router = express.Router();
 
-router.get('/', getPeople);
+// router.get('/', getPeople);
+// router.post('/', createPerson);
+// router.post('/postman', createPerson);
+// router.put('/:id', updatePerson);
+// router.delete('/:id', deletePerson);
 
-router.post('/', createPerson);
-
-router.post('/postman', createPerson);
-
-router.put('/:id', updatePerson);
-
-router.delete('/:id', deletePerson);
+router.route('/').get(getPeople).post(createPerson);
+router.route('/postman').post(createPersonPostman);
+router.route('/:id').put(updatePerson).delete(deletePerson);
 
 module.exports = router;
