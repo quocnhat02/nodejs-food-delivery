10ae90fee1bab3e6ec209506f3c15706c0530ec7
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-22 18:55:37 +0700

    express 54

--- app.js
+++ app.js
@@ -42,6 +42,19 @@ app.put('/api/people/:id', (req, res) => {
   const { name } = req.body;
 
   const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
