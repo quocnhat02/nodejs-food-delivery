755bf8583414f9f7d8389004cb44ec48010b12a5
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-22 22:49:13 +0700

    task manager api 4

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
+const tasks = require('./routes/tasks');
+
 const app = express();
 
 const port = 3000;
+
+// middleware
+app.use(express.json());
 
 // routes
 app.get('/hello', (req, res) => {
   return res.send('Task manager API');
 });
+
+app.use('/api/v1/tasks', tasks);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
