fbc6c96de0be6026fee1af61efbd6d24942b1903
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-22 21:44:16 +0700

    express 61

--- app.js
+++ app.js
@@ -13,15 +13,6 @@ app.use(express.json());
 
 app.use('/api/people', people);
 
-app.post('/login', (req, res) => {
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
-
-  return res.status(401).send('Please provide credentials');
-});
-
 app.listen(5000, () => {
   console.log('Server is listening on port 5000...');
 });
