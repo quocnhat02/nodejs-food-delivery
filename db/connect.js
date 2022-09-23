b8842a5d295948e0a0a28eb8b9b9e98b49f1e757
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-23 17:24:53 +0700

    task manager api 9

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const mongoURI = 'mongodb://localhost:27017/task-manager';
 
 mongoose
-  .connect(mongoURI)
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('CONNECTED TO THE DB...');
   })
