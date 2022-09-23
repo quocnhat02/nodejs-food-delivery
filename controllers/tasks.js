c5c6e043924bfef7928106cfe0415a2271cf7533
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-23 20:26:26 +0700

    task manager api 17

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -19,8 +19,19 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  return res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    return res.status(200).json({ task });
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
