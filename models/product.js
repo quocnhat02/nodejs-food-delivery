8f72041d5cdfa405ab49a0bac4a1d7445e112ddb
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-24 22:20:50 +0700

    store api 9

--- models/product.js
+++ models/product.js
@@ -23,6 +23,10 @@ const productSchema = new mongoose.Schema({
   },
   company: {
     type: String,
-    enum: ['ikea', 'liddy', 'caressa', 'marcos'],
+    enum: {
+      values: ['ikea', 'liddy', 'caressa', 'marcos'],
+      message: '{VALUE} is not supported',
+    },
+    // enum: ['ikea', 'liddy', 'caressa', 'marcos'],
   },
 });
