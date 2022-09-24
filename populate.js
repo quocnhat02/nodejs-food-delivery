88c6bcc9213c03a3900528faf8afa53590944acf
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-24 23:48:41 +0700

    store api 12

--- populate.js
+++ populate.js
@@ -8,6 +8,7 @@ const jsonProducts = require('./products.json');
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+    console.log('Success!!!');
   } catch (error) {
     console.log(error);
   }
