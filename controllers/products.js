79ccec7407f7b5427905f9201509dab319d6cd94
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-24 22:00:03 +0700

    store api 5

--- controllers/products.js
+++ controllers/products.js
@@ -2,11 +2,11 @@ const getAllProductsStatic = async (req, res) => {
   return res.status(200).json({ msg: 'Products testing route' });
 };
 
-const getProductsStatic = async (req, res) => {
+const getAllProducts = async (req, res) => {
   return res.status(200).json({ msg: 'Products route' });
 };
 
 module.exports = {
   getAllProductsStatic,
-  getProductsStatic,
+  getAllProducts,
 };
