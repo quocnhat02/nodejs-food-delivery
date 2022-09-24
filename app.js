6c758ccd2832d8e1de2f43128e98e17104f853b1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-24 22:01:00 +0700

    store api 6

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -19,7 +20,7 @@ app.get('/', (req, res) => {
   );
 });
 
-app.use('/api/v1/products');
+app.use('/api/v1/products', productsRouter);
 
 // products route
 app.use(notFoundMiddleware);
