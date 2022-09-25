f14a18e8a51060a004e4a0ca6ff21cb2e5b8be39
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-25 10:37:43 +0700

    store api 16

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,10 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
-  return res.status(200).json({ products });
+  const products = await Product.find({
+    featured: true,
+  });
+  return res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
