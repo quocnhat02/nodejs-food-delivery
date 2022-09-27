960a1c166e0b8a5c1142cd98f0a6ff58c9f17517
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-27 22:03:53 +0700

    modules 9

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
 
-const textOut = `This is what we know about the avocado ${textIn}\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut, 'utf-8');
-console.log('File written!');
+// const textOut = `This is what we know about the avocado ${textIn}\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut, 'utf-8');
+// console.log('File written!');
+
+// Non-locking, asynchronous way
+fs.readFile('./txt/start.txt');
