2ac317ac083d399d54aa583476601b6d3bf9fdaa
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-27 22:12:26 +0700

    modules 13

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const fs = require('fs');
 fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
     console.log(data2);
+
+    fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
+      console.log(data3);
+    });
   });
 });
 
