598b71b3c31a4df16086577975a2b89128f26c7e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-27 22:15:27 +0700

    modules 14

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
 
     fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
       console.log(data3);
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('Your file has been written');
+      });
     });
   });
 });
