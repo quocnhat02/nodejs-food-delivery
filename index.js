b6a62f170de716baf0dcb68d8cca4b036e3916d2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-27 21:44:50 +0700

    modules 8

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ console.log(textIn);
 
 const textOut = `This is what we know about the avocado ${textIn}\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut, 'utf-8');
+console.log('File written!');
