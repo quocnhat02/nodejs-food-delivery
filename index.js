83533609b94303e3c67002565667a9fba6c32977
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-27 21:43:21 +0700

    modules 6

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
 
-const textOut = `This is what we know about the avocado ${textIn}`;
+const textOut = `This is what we know about the avocado ${textIn}\nCreated on ${Date.now()}`;
