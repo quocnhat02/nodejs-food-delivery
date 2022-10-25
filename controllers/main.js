2338951ef77f7e4d476846cfffbf349dd4393347
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-25 22:59:44 +0700

    controller and route jwt 5

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,12 @@
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
   const { username, password } = req.body;
-  console.log(username, password);
+
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+
   res.send('Fake Login/Register/Signup');
 };
 
