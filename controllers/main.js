56ac6f750adf9547bf62c6ac1c016331cb20024a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-26 10:02:40 +0700

    jwt token 1

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
@@ -7,10 +8,18 @@ const login = async (req, res) => {
     throw new CustomAPIError('Please provide email and password', 400);
   }
 
-  res.send('Fake Login/Register/Signup');
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
+  console.log(req.headers);
+
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, John`,
