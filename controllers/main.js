53a48f54867498f5a842cb1113609b4270733e3f
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-26 10:12:34 +0700

    jwt token 3

--- controllers/main.js
+++ controllers/main.js
@@ -25,7 +25,14 @@ const dashboard = async (req, res) => {
   }
 
   const token = authHeader.split(' ')[1];
-  console.log(token);
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log(decoded);
+  } catch (error) {
+    throw new CustomAPIError('No authorized to access this route', 401);
+  }
 
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
