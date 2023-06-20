e804835d5dade55371fec4f2cf2c0fe1cc904a43
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-21 00:31:37 +0700

    folder 2.13

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,3 +1,5 @@
+import User from '../models/User';
+
 export class UserController {
   static login(req, res, next) {
     // const data = { name: 'Nhat' };
@@ -5,7 +7,21 @@ export class UserController {
     // (req as any).errorStatus = 422;
     // const error = new Error('User email or password does not match');
     // next(error);
-    res.send(req.query);
+    // res.send(req.query);
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = new User({ email, password });
+
+    user
+      .save()
+      .then((user) => {
+        res.status(201).send(user);
+      })
+      .catch((err) => {
+        next(err);
+      });
   }
 
   static test1(req, res, next) {
