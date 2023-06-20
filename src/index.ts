318d226125c7d8a9834418811d7c95b5da158bef
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-20 20:13:14 +0700

    folder 2.7

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,19 @@ app.listen(5000, () => {
   console.log('Server is running on port 5000');
 });
 
-app.get('/api/users/login', (req, res) => {
-  res.json({
-    name: 'Nhat',
-  });
+app.use((req, res, next) => {
+  console.log('middleware1');
+  next();
 });
+
+app.get(
+  '/api/users/login',
+  (req, res, next) => {
+    console.log('test');
+    next();
+  },
+  (req, res, next) => {
+    console.log('middleware2');
+    res.send('test2');
+  }
+);
