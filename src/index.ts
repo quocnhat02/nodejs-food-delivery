aa58e848f255443fa4163a38316e324239660490
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-06-20 20:07:47 +0700

    folder 2.6

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,8 @@ app.listen(5000, () => {
   console.log('Server is running on port 5000');
 });
 
-const object1 = { id: 1, name: 'Nhat' };
-const object2 = { ...object1, email: 'ae@gmail.com' };
-const object3 = object1;
-object1.id = 2;
-console.log('2', object2);
-console.log('3', object3);
+app.get('/api/users/login', (req, res) => {
+  res.json({
+    name: 'Nhat',
+  });
+});
