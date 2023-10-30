fe5d7e9be17c436f40042e537e70ce65b58b5806
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-10-30 12:28:21 +0700

    setup

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-
-const app: express.Application = express();
-
-const PORT = 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
